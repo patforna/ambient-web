76511dbdc5b6a5781b2b882516ea0dcdc11936ce
Marc Hofer <mhofer@thoughtworks.com>
2013-03-23 22:16:26 +0530

    Adding google analytics events for signup, facebook-link and twitter-link.

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ $(document).on('submit', '.call-to-action form', function(event) {
     $.post(this.action, $(this).serialize(), function(response) {
       var snippet = $($(response).find('.call-to-action')).first().html();
       callToActionBox.replaceWith(snippet);
+	  _gaq.push(['_trackEvent', 'action', 'signup']);
     })
 
     return false;
@@ -12,4 +13,13 @@ $(document).on('submit', '.call-to-action form', function(event) {
 
 $(document).on('closed', '.alert', function() {
   $('.call-to-action .prompt').removeClass('hidden');
-})
+});
+
+$(document).on('click', '.icon-facebook', function() {
+  _gaq.push(['_trackEvent', 'action', 'social', 'facebook']);
+});
+
+$(document).on('click', '.icon-twitter', function() {
+  _gaq.push(['_trackEvent', 'action', 'social', 'twitter']);
+});
+
